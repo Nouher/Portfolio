0887ca00595792ed1e90b4bc9b297129219adb56
unknown <nouhirh@gmail.com>
2022-03-20 13:12:08 +0000

    last commit

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -2,7 +2,6 @@ import React from 'react'
 import styled from 'styled-components'
 import { ImageProjectData } from '../data'
 import { mobile } from '../Responsive'
-import Footer from '../Components/Footer';
 
     
 const Container=styled.div`

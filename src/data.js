2438e43a1b98847a73654f6bf9f57e31ccd72be5
unknown <nouhirh@gmail.com>
2022-04-28 20:38:01 +0000

    last commit

--- src/data.js
+++ src/data.js
@@ -1,92 +1,94 @@
-import img1 from './assets/project/project-image01.png'
-import img2 from './assets/project/project-image02.png'
-import img3 from './assets/project/project-image03.png'
-import img4 from './assets/project/project-image04.png'
+import img1 from "./assets/project/project-image01.png";
+import img2 from "./assets/project/project-image02.png";
+import img3 from "./assets/project/project-image03.png";
+import img4 from "./assets/project/project-image04.png";
+import img5 from "./assets/project/project-image05.png";
 
-export const EducationsItems=[
+export const EducationsItems = [
+  {
+    id: 1,
+    title: "Software development technician",
+    date: 2021,
+    desc: "Specialized Institute of Applied Technology NTIC BOUZNIKA",
+    end: false,
+  },
 
-    {
-        id:1,
-        title:"Software development technician",
-        date:2021,
-        desc:"Specialized Institute of Applied Technology NTIC BOUZNIKA",
-        end:false
-    },
-   
-    {
-        id:3,
-        title:"Office Excel",
-        date:2021,
-        desc:"Microsoft Office Specialized",
-        end:false
-    },
-    {
-        id:4,
-        title:"IT Essentials",
-        date:2020,
-        desc:"Cisco Networking Academy",
-        end:false
-    }, 
-    {
-        id:2,
-        title:"Physical Science BACCALAUREATE",
-        date:2019,
-        desc:"El Mansouria Qualified Secondary School",
-        end:true
-    },
-    
-]
+  {
+    id: 3,
+    title: "Office Excel",
+    date: 2021,
+    desc: "Microsoft Office Specialized",
+    end: false,
+  },
+  {
+    id: 4,
+    title: "IT Essentials",
+    date: 2020,
+    desc: "Cisco Networking Academy",
+    end: false,
+  },
+  {
+    id: 2,
+    title: "Physical Science BACCALAUREATE",
+    date: 2019,
+    desc: "El Mansouria Qualified Secondary School",
+    end: true,
+  },
+];
 
-export const ExperiencesItems=[
-    {
-        id:1,
-        title:"Ecommerce Website",
-        date:2021,
-        desc:"creation of an e-commerce web application Backend and Frontend using React, Redux, NodeJs, MongoDB",
-        end:false
-    },
-    {
-        id:2,
-        title:"Creation Aplication Management WinForms",
-        date:2020,
-        desc:"create management application check follow-up a cheque using C# and SQL Server",
-        end:false
-    },
-    {
-        id:3,
-        title:"Create a taxi property application",
-        date:2020,
-        desc:"Create an application to enter the ownership of taxis C# and SQL Server",
-        end:true
-    },
-   
-   
-]
+export const ExperiencesItems = [
+  {
+    id: 1,
+    title: "Ecommerce Website",
+    date: 2021,
+    desc: "creation of an e-commerce web application Backend and Frontend using React, Redux, NodeJs, MongoDB",
+    end: false,
+  },
+  {
+    id: 2,
+    title: "Creation Aplication Management WinForms",
+    date: 2020,
+    desc: "create management application check follow-up a cheque using C# and SQL Server",
+    end: false,
+  },
+  {
+    id: 3,
+    title: "Create a taxi property application",
+    date: 2020,
+    desc: "Create an application to enter the ownership of taxis C# and SQL Server",
+    end: true,
+  },
+];
 
-
-export const ImageProjectData=[
-    {
-        id:1,
-        name:"Ecommerce Website",
-        url:img1,
-        link:'https://shophz.netlify.app/'
-    },
-    {
-        id:2,
-        name:"Management Application",
-        url:img2,
-        link:''
-    },
-    {
-        id:3,
-        name:"Management Application",
-        url:img3,
-        link:''
-    },
-    {
-        id:4,
-        name:"Management Application",
-        url:img4,
-        link:''
-    },
-]
+export const ImageProjectData = [
+  {
+    id: 1,
+    name: "Ecommerce Website",
+    url: img1,
+    link: "https://shophz.netlify.app/",
+  },
+  {
+    id: 2,
+    name: "Marbella",
+    url: img2,
+    link: "https://bestservicesmarbella.netlify.app/",
+  },
+  {
+    id: 3,
+    name: "BSITEAM",
+    url: img3,
+    link: "https://bsiteam.ma/",
+  },
+  {
+    id: 4,
+    name: "Management Application",
+    url: img4,
+    link: "",
+  },
+  {
+    id: 5,
+    name: "Management Application",
+    url: img5,
+    link: "",
+  },
+];
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,54 +1,54 @@
-import React from 'react'
-import styled from 'styled-components'
-import { ImageProjectData } from '../data'
-import { mobile } from '../Responsive'
+import React from "react";
+import styled from "styled-components";
+import { ImageProjectData } from "../data";
+import { mobile } from "../Responsive";
 
-    
-const Container=styled.div`
-    width: 100%;
-    display: grid;
-    grid-template-columns: repeat(4,1fr);
-    height: 100vh;
-    
-    ${mobile({gridTemplateColumns:'repeat(1,1fr)',paddingTop:100})};
-    
-`
-const ProjectContainer=styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    ${mobile({paddingBottom:30})};
-
-`
-const Title=styled.h1`
- text-align: center;
- font-size: 18px;
-`
-const Image=styled.img`
-    height: 250px;
-`
-const Link=styled.a`
-   color: #555;
-   text-decoration: none;
-   font-weight: bold;
-   font-size: 18px;
-`
-
+const Container = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  height: 100vh;
+  padding: 100px 0;
+  ${mobile({ gridTemplateColumns: "repeat(1,1fr)", paddingTop: 100 })};
+`;
+const ProjectContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  ${mobile({ paddingBottom: 30 })};
+`;
+const Title = styled.h1`
+  text-align: center;
+  font-size: 18px;
+`;
+const Image = styled.img`
+  height: 200px;
+  margin: 15px 0;
+  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+  border-radius: 5px;
+`;
+const Link = styled.a`
+  color: #555;
+  text-decoration: none;
+  font-weight: bold;
+  font-size: 18px;
+`;
 
 const Projects = () => {
+  return (
+    <Container>
+      {ImageProjectData.map((item) => (
+        <ProjectContainer>
+          <Title>{item.name}</Title>
+          <Image src={item.url}></Image>
+          <Link href={item.link} target="_blank">
+            View Project
+          </Link>
+        </ProjectContainer>
+      ))}
+    </Container>
+  );
+};
 
-    return (
-        <Container>
-            {ImageProjectData.map(item=>(
-                <ProjectContainer>
-                    <Title>{item.name}</Title>
-                    <Image src={item.url}></Image>
-                    <Link href={item.link} target='_blank'>View Project</Link>
-                </ProjectContainer>
-            ))}
-        </Container>
-    )
-}
-
-export default Projects
+export default Projects;
